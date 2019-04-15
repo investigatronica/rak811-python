6b304a68fedc2c29f45523a32f74a3ac3fd886e8
investigatronica <gax@openmailbox.org>
2019-04-15 12:47:33 -0300

    camandos para usar en la lib

--- uart.py
+++ uart.py
@@ -10,3 +10,15 @@ print(str(uart.readline()).split("\\")[0])
 print(uart.write(str.encode("at+gpio=PA15,1\r\n")))
 print(str(uart.readline()).split("OK")[1].split("\\")[0])
 uart.write(str.encode("at+gpio=15,1\r\n"))  #nro de pin del RAK811 LoRa Module y no del beakboard
+
+uart.write(str.encode("at+set_config=app_eui:70B3D57ED001ABFD&app_key:EEAD3B45BF0983C4AAA38A6A3CCB7263\r\n"))
+print(uart.readline())
+uart.write(str.encode("at+join=otaa\r\n"))
+print(uart.readline())
+
+binascii.hexlify(string.encode()).decode("utf-8")
+
+uart.write(str.encode("at+join=otaa\r\n"))
+uart.write(str.encode("at+dr=4\r\n"))
+uart.write(str.encode("at+send=0,1,63686175\r\n"))
+print(uart.read())
